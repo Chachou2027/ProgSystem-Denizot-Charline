22a5db76b1f3eb0e2ccbb7b573f9471be419e554
Charline Denizot <denizotcharline@gmail.com>
2026-09-24 16:46:42 +0200

    writeLong + readLong terminés et tests ok

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -32,5 +32,32 @@ public class Utils {
         return (short) (b1 + b2);
     }
 	
+	public static long writeLong(byte[] memory, int offset, long value) {
+
+		memory[offset] = (byte) ((value >> 56) & 0xFF);
+		memory[offset + 1] = (byte) ((value >> 48) & 0xFF);
+		memory[offset + 2] = (byte) ((value >> 40) & 0xFF);
+		memory[offset + 3] = (byte) ((value >> 32) & 0xFF);
+		memory[offset + 4] = (byte) ((value >> 24) & 0xFF);
+		memory[offset + 5] = (byte) ((value >> 16) & 0xFF);
+		memory[offset + 6] = (byte) ((value >> 8) & 0xFF);
+		memory[offset + 7] = (byte) ((value) & 0xFF);
+				
+        return 8;
+    }
+	
+	public static long readLong(byte[] memory, int offset) {
+		long b1 =  ((long)(memory[offset] & 0xFF ) << 56);
+		long b2 =  ((long)(memory[offset + 1] & 0xFF )<< 48);
+		long b3 =  ((long)(memory[offset + 2] & 0xFF )<< 40);
+		long b4 =  ((long)(memory[offset + 3] & 0xFF )<< 32);
+		long b5 =  ((long)(memory[offset + 4] & 0xFF )<< 24);
+		long b6 =  ((long)(memory[offset + 5] & 0xFF )<< 16);
+		long b7 =  ((long)(memory[offset + 6] & 0xFF )<< 8);
+		long b8 =  ((long)memory[offset + 7] & 0xFF);
+		
+        return (long) (b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8);
+    }
+	
 	
 }
